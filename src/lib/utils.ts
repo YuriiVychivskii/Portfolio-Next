5efff4986ca3344b30b85398f82a53ab1a0995ff
Yura Vyvhivskii <yuravychivskii@gmail.com>
2025-05-15 14:56:48 +0300

    fix: change eu-US to en-US

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: string) {
-	return new Date(date).toLocaleDateString('eu-US', {
+	return new Date(date).toLocaleDateString('en-US', {
 		day: 'numeric',
 		month: 'long',
 		year: 'numeric',
