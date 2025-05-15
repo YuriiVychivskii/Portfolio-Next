7432533d228265af4d50f8939e8ec4100df6c87a
Yura Vyvhivskii <yuravychivskii@gmail.com>
2025-05-15 15:04:55 +0300

    feat: add search posts

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,4 +1,4 @@
-import Posts from '@/components/posts';
+import PostsWithSearch from '@/components/posts-with-search';
 import { getPosts } from '@/lib/posts';
 
 export default async function PostsPage() {
@@ -9,7 +9,7 @@ export default async function PostsPage() {
 			<div className='container max-w-3xl'>
 				<h1 className='title mb-12'>Posts</h1>
 
-				<Posts posts={posts} />
+				<PostsWithSearch posts={posts} />
 			</div>
 		</section>
 	);
