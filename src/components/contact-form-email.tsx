cee97dff59b5709e68ac8872953521ccf0fb8269
Yura Vyvhivskii <yuravychivskii@gmail.com>
2025-05-21 19:04:53 +0300

    feat: add email form tamplate

---
+++ src/components/contact-form-email.tsx
@@ -0,0 +1,21 @@
+interface EmailTemplateProps {
+	name: string;
+	email: string;
+	message: string;
+}
+
+export const ContactFormEmail: React.FC<Readonly<EmailTemplateProps>> = ({
+	name,
+	email,
+	message,
+}) => (
+	<div>
+		<h1>Contact form submission</h1>
+		<p>
+			From <strong>{name}</strong> at {email}
+		</p>
+
+		<h2>Message:</h2>
+		<p>{message}</p>
+	</div>
+);
