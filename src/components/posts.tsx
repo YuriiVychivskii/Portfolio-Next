759ad7c1f3fb2edd466ea898b1539b9717aef4cc
Yura Vyvhivskii <yuravychivskii@gmail.com>
2025-05-22 21:09:25 +0300

    fix: change link styles

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -11,7 +11,7 @@ export default function Posts({ posts }: { posts: PostMetadata[] }) {
 				<li key={post.slug}>
 					<Link
 						href={`/posts/${post.slug}`}
-						className='flex flex-col justify-between gap-x-4 gap-y-1'
+						className='flex justify-between gap-x-4 gap-y-1'
 					>
 						<div className='max-w-lg'>
 							<p className='text-lg font-semibold'>{post.title}</p>
