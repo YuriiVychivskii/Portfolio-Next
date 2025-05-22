a77d6deec5905248919e6aa208dda4913efcd8cf
Yura Vyvhivskii <yuravychivskii@gmail.com>
2025-05-22 21:06:30 +0300

    fix: change text about me

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -3,19 +3,13 @@ import Image from 'next/image';
 
 export default function Intro() {
 	return (
-		<section className='flex flex-col-revers items-center gap-x-10 gap-y-4 '>
+		<section className='flex flex-col-revers items-center gap-x-10 gap-y-4 pb-24'>
 			<div className='mt-2 flex-1 md:mt-0'>
 				<h1 className='title no-underline'>Hey, I`m Yurii.</h1>
 				<p className='mt-3 font-light text-muted-foreground'>
 					I'm a Junior Frontend Developer based in Ivano-Frankivsk, Ukraine. I
 					specialize in building modern, responsive web interfaces using React,
-					Next.js, and TypeScript. I have experience developing pet projects
-					with authentication, external API integration, database interaction,
-					and state management with Redux. I pay special attention to project
-					structure, clean and maintainable code, and styling with SCSS or
-					Tailwind CSS. I follow best practices such as SOLID, DRY, and KISS
-					principles. I'm open to collaboration and constantly improving my
-					skills.
+					Next.js, and TypeScript.
 				</p>
 			</div>
 
