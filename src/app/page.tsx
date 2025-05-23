98b94bd938ee270cda21d3b929ddbb08a0759143
Yura Vyvhivskii <yuravychivskii@gmail.com>
2025-05-23 15:27:29 +0300

    feat: add Intro, Recent posts and projects sections, NewsLetterForm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import Intro from '@/components/intro';
+import NewsLetterForm from '@/components/news-letter-form';
+import RecentPosts from '@/components/recent-posts';
+import RecentProjects from '@/components/recent-projects';
+
 export default function Home() {
 	return (
 		<section className='py-24'>
-			<div className='container'>
-				<h1 className='text-3xl font-bold'></h1>
+			<div className='container max-w-3xl'>
+				<Intro />
+
+				<RecentPosts />
+				<RecentProjects />
+				<NewsLetterForm />
 			</div>
 		</section>
 	);
