08616ee21b6a391029ef39cf562fc627a5520299
Yura Vyvhivskii <yuravychivskii@gmail.com>
2025-06-13 13:31:22 +0300

    feat: add subscribe form logic

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -4,7 +4,7 @@ import { Resend } from 'resend';
 import { z } from 'zod';
 
 import { ContactFormEmail } from '@/components/contact-form-email';
-import { ContactFormSchema } from './schemas';
+import { ContactFormSchema, NewsletterFormSchema } from './schemas';
 
 type ContactFormInputs = z.infer<typeof ContactFormSchema>;
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -29,11 +29,36 @@ export async function sendEmail(data: ContactFormInputs) {
 			react: await ContactFormEmail({ name, email, message }),
 		});
 
-		console.log('emailData>>', emailData);
-		console.log('error>>', error);
-
 		if (!emailData || error) {
 			throw new Error('Failed to send email');
+		}
+
+		return { success: true };
+	} catch (error) {
+		return { error };
+	}
+}
+
+export async function subscribe(data: { email: string }) {
+	const { success, data: email } = NewsletterFormSchema.safeParse(data);
+
+	if (!success || !email.email) {
+		throw new Error('Failed to send email');
+	}
+
+	try {
+		const { error } = await resend.emails.send({
+			from: 'Yurii Vychivskii <work@yurii-vychivskii.com>',
+			to: email.email,
+			subject: 'You have subscribed to project updates!',
+			html: `
+				<h1>Thank you for subscribing!</h1>
+				<p>We'll keep you informed about important updates and news.</p>
+				`,
+		});
+
+		if (error) {
+			return { error };
 		}
 
 		return { success: true };
